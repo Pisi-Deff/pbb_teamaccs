bd295dc6a1ecd6d820acc4ede8f60afe6cc10304
Eerik Mägi <eerik46@gmail.com>
2013-12-18 21:50:22 +0200

    Implement list of new gameservers.

---
+++ classes/Server.class.php
@@ -0,0 +1,66 @@
+<?php
+class Server {
+	private $id;
+	
+	public function __construct($id) {
+		$this->id = $id;
+	}
+	
+	public function db_confirm() {
+		try {
+			$dbh = Database::getInstance()->getDatabaseHandle();
+			$stmt = $dbh->prepare('SELECT f_kinnita_mänguserver (?)');
+			$stmt->execute(array($this->id));
+		} catch (PDOException $e) {
+			Page::addMessage(new Message($e->getMessage(), 'error'));
+		}
+	}
+	
+	public function db_delete() {
+		try {
+			$dbh = Database::getInstance()->getDatabaseHandle();
+			$stmt = $dbh->prepare(
+					'DELETE FROM Mänguserver WHERE Mänguserver_ID = ?');
+			$stmt->execute(array($this->id));
+		} catch (PDOException $e) {
+			Page::addMessage(new Message($e->getMessage(), 'error'));
+		}
+	}
+	
+	public static function db_getList($teamAccountID = null) {
+		$servers = array();
+		try {
+			$dbh = Database::getInstance()->getDatabaseHandle();
+			$sql = 'SELECT * FROM Mänguserverite_nimekiri';
+			if ($teamAccountID !== null) {
+				$sql .= ' WHERE Rühmakonto_ID = ?';
+			}
+			$stmt = $dbh->prepare($sql);
+			if ($teamAccountID !== null) {
+				$stmt->bind_param(1, $teamAccountID);
+			}
+			$stmt->execute();
+			while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+				$servers[$row['mänguserver_id']] = $row;
+			}
+		} catch (PDOException $e) {
+			Page::addMessage(new Message($e->getMessage(), 'error'));
+		}
+		return $servers;
+	}
+	
+	public static function db_getListNew() {
+		$servers = array();
+		try {
+			$dbh = Database::getInstance()->getDatabaseHandle();
+			$stmt = $dbh->query(
+					'SELECT * FROM Uute_mänguserverite_nimekiri');
+			while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+				$servers[$row['mänguserver_id']] = $row;
+			}
+		} catch (PDOException $e) {
+			Page::addMessage(new Message($e->getMessage(), 'error'));
+		}
+		return $servers;
+	}
+}
--- pages/employee/NewServersEmployeePage.class.php
+++ pages/employee/NewServersEmployeePage.class.php
@@ -2,6 +2,50 @@
 class NewServersEmployeePage extends EmployeePage {
 	public function setupEmployee() {
 		$this->setTitle('Uued mänguserverid');
-		// todo
+		if (!empty($this->post['serverid']) && is_numeric($this->post['serverid']) &&
+				($id = intval($this->post['serverid'])) > 0) {
+			$server = new Server($id);
+			if (!empty($this->post['confirmserver'])) {
+				$server->db_confirm();
+			} else if (!empty($this->post['denyserver'])) {
+				$server->db_delete();
+			}
+		}
+		$this->content .= $this->genNewServersTable();
+	}
+	
+	public function genNewServersTable() {
+		$table = <<<ENDCONTENT
+<table>
+	<tr class="toprow">
+		<td>IP:port</td>
+		<td>Mäng</td>
+		<td>Rühma nimi</td>
+		<td></td>
+	</tr>
+ENDCONTENT;
+		$newServers = Server::db_getListNew();
+		if (!empty($newServers)) {
+			foreach ($newServers as $server) {
+				$table .= <<<ENDCONTENT
+	<tr>
+		<td>{$server['ip']}:{$server['port']}</td>
+		<td>{$server['mängu_nimi']}</td>
+		<td>{$server['rühma_nimi']}</td>
+		<td>
+			<form method="POST">
+				<input type="hidden" name="serverid" value="{$server['mänguserver_id']}" />
+				<input class="button" type="submit" name="confirmserver" value="Kinnita" />
+				<input class="button" type="submit" name="denyserver" value="Kustuta" />
+			</form>
+		</td>
+	</tr>		
+ENDCONTENT;
+			}
+		} else {
+			$table .= '<tr><td colspan="4">Uusi mänguservereid ei ole</td></tr>';
+		}
+		$table .= "</table>\n";
+		return $table;
 	}
 }
