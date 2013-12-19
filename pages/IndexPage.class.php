87b71f23281af6250470ccf90bd6b28ec864d783
Eerik Mägi <eerik46@gmail.com>
2013-12-20 01:36:16 +0200

    Replace placeholder text on index page.

--- pages/IndexPage.class.php
+++ pages/IndexPage.class.php
@@ -3,7 +3,10 @@ class IndexPage extends UIPage {
 	public function setup() {
 		$this->content = <<<ENDCONTENT
 <div class="content">
-	hello world.
+	Teretulemast PBBans-i lehele.<br />
+	<br />
+	Hetkel on realiseeritud ainult rühmakontode haldaja roll.<br />
+	Selle kasutamiseks sisenege ülal töötajatepaneeli.
 </div>
 ENDCONTENT;
 	}
